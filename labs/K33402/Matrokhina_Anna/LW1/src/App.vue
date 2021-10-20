8e7d43b51bc49406669bd39ad18638f9769cf198
meowberry <ann.matrokhina@gmail.com>
2021-10-21 02:25:04 +0300

    Fix navbar container

--- labs/K33402/Matrokhina_Anna/LW1/src/App.vue
+++ labs/K33402/Matrokhina_Anna/LW1/src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div id="app">
-      <b-navbar toggleable="sm" type="dark" variant="dark">
-          <b-navbar-brand :to="{name: 'Home'}">Прогноз погоды</b-navbar-brand>
+    <b-navbar toggleable="sm" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-brand :to="{name: 'Home'}">Прогноз погоды</b-navbar-brand>
 
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-          <b-collapse id="nav-collapse" is-nav>
-              <!-- Right aligned nav items -->
-              <b-navbar-nav class="ml-auto">
-                  <b-nav-item :to="{name: 'Registration'}">
-                    Регистрация
-                  </b-nav-item>
-                  <b-nav-item :to="{name: 'Authorization'}">
-                    Авторизация
-                  </b-nav-item>
-              </b-navbar-nav>
-          </b-collapse>
-      </b-navbar>
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item :to="{name: 'Registration'}">
+              Регистрация
+            </b-nav-item>
+            <b-nav-item :to="{name: 'Authorization'}">
+              Авторизация
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
 
     <main>
-      <router-view/>
+      <router-view />
     </main>
 
   </div>
