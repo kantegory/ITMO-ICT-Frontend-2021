cf74928fbd0d937150222368bebfd8afc06c4cd6
marysadness <56173118+marysadness@users.noreply.github.com>
2021-11-24 01:26:10 +0300

    added images to city

--- labs/K33402/Karatetskaya Maria/LW2/js/tickets.js
+++ labs/K33402/Karatetskaya Maria/LW2/js/tickets.js
@@ -1,41 +1,51 @@
 async function getTickets() {
     const new_url = new URLSearchParams(window.location.search);
-    document.write(document.referrer);
-
-    let from = 'Omsk';
-    let to = 'Moskow';
-    let date = '2022-01-12';
-    let fro = new_url.get("from");
-    console.log(fro);
+    let to = new_url.get(`to`);
+    let date = new_url.get(`date`);
+    let from = new_url.get(`from`);
+    console.log(new_url);
     let url = `http://localhost:3000/tickets?from=${from}&to=${to}&date=${date}`;
     let res = await fetch(url);
     let data = await res.json();
     console.log(data);
     return data;
 }
+async function getPass(){
+    const new_url = new URLSearchParams(window.location.search);
+    let pass = new_url.get(`pass`);
+    return pass;
+}
+
 async function sendTickets() {
     let tickets = await getTickets();
-    if (!tickets) {
-    console.log("нет");}
-    let html = "<div class='container'> <h2 class='text-center'>Результаты поиска</h2></div>";
-    let htmlSegment = ""
-    if (tickets) {
-        html += '<div class="container">';
-        html += '<div class="row">';
-        htmlSegment = `<div>
-        <p>Откуда ${tickets[0].from}</p>
-        <p>Куда ${tickets[0].to}</p>
-        <p>Дата вылета ${tickets[0].date}</p>
-        </div>`;
-        html += htmlSegment;
-        html += '</div>';
-        html += '</div>';
+    let pass = await getPass();
+    let html = "";
+    let htmlDif = "";
+    let htmlImg = "";
+    console.log(pass);
+    html += "<div class='title'><a>Билеты</a></div>";
+    html += "<div class='row card-group form-fly'>";
+    for (let i = 0; i < tickets.length; i++){
+    if (tickets[i].pass >= pass){
+    let price_all = pass * tickets[i].price;
+
+    html += "<div class='col-lg-4 col-md-6 col-sm-12 col-card'>";
+    htmlImg = `<div class='card'><img src='images/${tickets[i].to}.jpg' class='card-img-top' alt=''>`;
+    html += htmlImg;
+    html += "<div class='card-body'>";
+    htmlDif = `<h5 class='card-title'>${tickets[i].from} - ${tickets[i].to}</h5>
+    <p class='card-text'>Количество пассажиров - ${pass}</p>
+    <p class='card-text'>Цена - ${price_all} руб</p>
+    <p class='card-text'>Дата вылета - ${tickets[i].date}</p>
+    <p class='card-text'>Время вылета - ${tickets[i].time}</p>`;
+    html += htmlDif;
+    html += "<button class='btn btn-primary little'><a href='#' data-toggle='modal' data-target='#myModal' >Купить</a></button></div></div></div>"
     }
+    }
+    html += "</div>"
     console.log(tickets);
     let container = document.querySelector('.tickets');
     container.innerHTML = html;
-
-
 };
 
 sendTickets();
