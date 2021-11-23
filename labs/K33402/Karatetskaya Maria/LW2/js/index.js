93f3cf79de2aa882390d649c2b133a97ddf1820c
marysadness <56173118+marysadness@users.noreply.github.com>
2021-11-23 23:02:30 +0300

    done LW2

--- labs/K33402/Karatetskaya Maria/LW2/js/index.js
+++ labs/K33402/Karatetskaya Maria/LW2/js/index.js
@@ -1,17 +1,23 @@
 function search(){
+    this.event.preventDefault()
     let from = document.getElementById("from").value;
     let to = document.getElementById("to").value;
     let date = document.getElementById("date").value;
+    let pass = document.getElementById("pass").value;
+    pass = pass.split()[0];
     let ans = "tickets.html";
     let param = '?';
     if (from) {
-    param += 'from=${from}'
+    param += `from=${from}`
     }
     if (to) {
-    param += '&to=${to}'
+    param += `&to=${to}`
     }
     if (date) {
-    param += '&date=${date}'
+    param += `&date=${date}`
+    }
+    if (pass) {
+    param += `&pass=${pass}`
     }
    window.location.href = ans + param;
 }
