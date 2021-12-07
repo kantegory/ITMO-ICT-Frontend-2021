580f4e1f134b50859855bc35075c5fc2703077e3
semenkulemin <skulemin@icloud.com>
2021-12-07 14:08:00 +0300

    Update JS and home.html

--- labs/K33401/Kulemin_Semyon/LW2/js/api_search.js
+++ labs/K33401/Kulemin_Semyon/LW2/js/api_search.js
@@ -3,7 +3,7 @@ let api_key = '5fa8289d2a7fa01e858bb2efe6a2b11b'
 fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+'55.75222'+'&lon='+'37.61556'+'&exclude={daily}'+'&appid='+api_key+'&lang=ru&units=metric')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        document.querySelector('#c1_temp_now').innerHTML = Math.round(data['daily'][0]['temp']['day']) + '&degC';
         document.querySelector('#c1_temp_day1').innerHTML = Math.round(data['daily'][0]['temp']['day']) + '&degC';
         document.querySelector('#c1_temp_day2').innerHTML = Math.round(data['daily'][1]['temp']['day']) + '&degC';
         document.querySelector('#c1_temp_day3').innerHTML = Math.round(data['daily'][2]['temp']['day']) + '&degC';
@@ -15,7 +15,7 @@ fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+'55.75222'+'&lon='+
 fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+'51.51279'+'&lon='+'-0.09184'+'&exclude={daily}'+'&appid='+api_key+'&lang=ru&units=metric')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        document.querySelector('#c2_temp_now').innerHTML = Math.round(data['daily'][0]['temp']['day']) + '&degC';
         document.querySelector('#c2_temp_day1').innerHTML = Math.round(data['daily'][0]['temp']['day']) + '&degC';
         document.querySelector('#c2_temp_day2').innerHTML = Math.round(data['daily'][1]['temp']['day']) + '&degC';
         document.querySelector('#c2_temp_day3').innerHTML = Math.round(data['daily'][2]['temp']['day']) + '&degC';
@@ -28,7 +28,7 @@ function location_success(pos) {
     fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+pos.coords.latitude+'&lon='+pos.coords.longitude+'&exclude={daily}'+'&appid='+api_key+'&lang=ru&units=metric')
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            document.querySelector('#c_temp_now').innerHTML = Math.round(data['daily'][0]['temp']['day'])  + '&degC';
             document.querySelector('#c_temp_day1').innerHTML = Math.round(data['daily'][0]['temp']['day']) + '&degC';
             document.querySelector('#c_temp_day2').innerHTML = Math.round(data['daily'][1]['temp']['day']) + '&degC';
             document.querySelector('#c_temp_day3').innerHTML = Math.round(data['daily'][2]['temp']['day']) + '&degC';
