ad76028f307014c22d18d60f7562b4591d3f5b1e
semenkulemin <skulemin@icloud.com>
2021-12-07 18:03:51 +0300

    Add a button to change the style and js file

--- practice/K33401/Kulemin_Semyon/PW3/js/api_search.js
+++ practice/K33401/Kulemin_Semyon/PW3/js/api_search.js
@@ -1,6 +1,6 @@
 let api_key = '5fa8289d2a7fa01e858bb2efe6a2b11b'
 
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+'55.75222'+'&lon='+'37.61556'+'&exclude={daily}'+'&appid='+api_key+'&lang=ru&units=metric')
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + '55.75222' + '&lon=' + '37.61556' + '&exclude={daily}' + '&appid=' + api_key + '&lang=ru&units=metric')
     .then(response => response.json())
     .then(data => {
         document.querySelector('#c1_temp_now').innerHTML = Math.round(data['daily'][0]['temp']['day']) + '&degC';
@@ -12,7 +12,7 @@ fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+'55.75222'+'&lon='+
         document.querySelector('#c1_cond_day3').innerHTML = data['daily'][2]['weather'][0]['description']
     })
 
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+'51.51279'+'&lon='+'-0.09184'+'&exclude={daily}'+'&appid='+api_key+'&lang=ru&units=metric')
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + '51.51279' + '&lon=' + '-0.09184' + '&exclude={daily}' + '&appid=' + api_key + '&lang=ru&units=metric')
     .then(response => response.json())
     .then(data => {
         document.querySelector('#c2_temp_now').innerHTML = Math.round(data['daily'][0]['temp']['day']) + '&degC';
@@ -25,10 +25,10 @@ fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+'51.51279'+'&lon='+
     })
 
 function location_success(pos) {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+pos.coords.latitude+'&lon='+pos.coords.longitude+'&exclude={daily}'+'&appid='+api_key+'&lang=ru&units=metric')
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude + '&exclude={daily}' + '&appid=' + api_key + '&lang=ru&units=metric')
         .then(response => response.json())
         .then(data => {
-            document.querySelector('#c_temp_now').innerHTML = Math.round(data['daily'][0]['temp']['day'])  + '&degC';
+            document.querySelector('#c_temp_now').innerHTML = Math.round(data['daily'][0]['temp']['day']) + '&degC';
             document.querySelector('#c_temp_day1').innerHTML = Math.round(data['daily'][0]['temp']['day']) + '&degC';
             document.querySelector('#c_temp_day2').innerHTML = Math.round(data['daily'][1]['temp']['day']) + '&degC';
             document.querySelector('#c_temp_day3').innerHTML = Math.round(data['daily'][2]['temp']['day']) + '&degC';
---
+++ practice/K33401/Kulemin_Semyon/PW3/js/style.js
@@ -0,0 +1,13 @@
+let style_button = document.querySelector('#style_button')
+let is_light = true
+style_button.addEventListener('click', function () {
+    console.log('click', is_light)
+    if (is_light) {
+        document.querySelector('#theme_style').setAttribute('href', '../css/dark_theme.css')
+        is_light = false
+    } else {
+        document.querySelector('#theme_style').setAttribute('href', '../css/light_theme.css')
+        is_light = true
+    }
+})
+// document.querySelector('#theme_style').getAttribute('href') === '../css/light_theme.css'
