068df8b8ea1409cbcc5f3621fec202c72a21a4c9
MatveyVlasov <vlasovmatvey007@gmail.com>
2021-12-21 19:00:09 +0300

    href changed to router

--- labs/K33402/Vlasov_Matvey/LW3/lab3/src/components/NavBar.vue
+++ labs/K33402/Vlasov_Matvey/LW3/lab3/src/components/NavBar.vue
@@ -1,22 +1,22 @@
 <template>
      <header>
         <b-navbar  toggleable="md" fixed="top">
-            <b-navbar-brand href="/">Home4Night</b-navbar-brand>
+            <b-navbar-brand href="javascript:void(0)" @click="$router.push('/')">Home4Night</b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
                     <theme-toggler />
-                    <b-nav-item href="/">Home</b-nav-item>
-                    <b-nav-item href="search">
+                    <b-nav-item href="javascript:void(0)" @click="$router.push('/')">Home</b-nav-item>
+                    <b-nav-item href="javascript:void(0)" @click="$router.push('search')">
                         <b-icon icon='search' font-scale='0.99'></b-icon>
                         Search
                     </b-nav-item>
                 </b-navbar-nav>
 
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-item href="login">Log In</b-nav-item>
-                    <b-nav-item href="signup">Sign Up</b-nav-item>
+                    <b-nav-item href="javascript:void(0)" @click="$router.push('login')">Log in</b-nav-item>
+                    <b-nav-item href="javascript:void(0)" @click="$router.push('signup')">Sign up</b-nav-item>
                 </b-navbar-nav>
             </b-collapse>
         </b-navbar>
