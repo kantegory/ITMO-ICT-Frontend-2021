b71bde5119c7ec7db3791be66f0e04fe618ab996
Andrei Poliakov <odyvannnn@gmail.com>
2021-12-27 21:05:21 +0300

    Добавил 3 лабораторную + 4 практическую

---
+++ labs/K33402/Polyakov_Andrei/LW3/lw3_weather/src/views/Log.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header/>
+    <Login/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Login from '@/components/Login'
+export default {
+  name: 'Log',
+  components: {
+    Login,
+    Header
+  }
+}
+</script>
