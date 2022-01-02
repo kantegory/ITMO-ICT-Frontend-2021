ac5e5303f840bcbf07745eaaaf2c88c2778b20a2
meowberry <ann.matrokhina@gmail.com>
2022-01-02 15:56:59 +0300

    Upload code from LW1

---
+++ labs/K33402/Matrokhina_Anna/LW2/src/components/WeatherCity.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-card :title="city" class="weather-card">
+    <b-card-text>
+      <p class="mt-4">
+        <b-icon-brightness-high /> Температура: {{ weather }}
+      </p>
+      <p>
+        <i class="bi bi-thermometer-half"></i> Давление: {{ pressure }}
+      </p>
+      <p>
+        <i class="bi bi-wind"></i> {{ windDirection }}, {{ wind }} м/с</p>
+
+      <div class="d-flex justify-content-center">
+        <b-button v-if="saveButton" size="sm" variant="outline-dark">Сохранить</b-button>
+        <b-button v-else size="sm" variant="outline-danger">Удалить</b-button>
+      </div>
+    </b-card-text>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'WeatherCity',
+  props: {
+    city: String,
+    weather: String,
+    pressure: Number,
+    wind: String,
+    windDirection: String,
+    saveButton: Boolean,
+  }
+}
+</script>
---
+++ labs/K33402/Matrokhina_Anna/LW2/src/views/Registration.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="registration-page">
+    <b-container>
+      <div class="auth-card-wrapper">
+        <b-card class="auth-card"
+                title="Регистрация"
+        >
+          <b-form>
+            <b-form-group label="Логин">
+              <b-form-input placeholder="Введите логин" />
+            </b-form-group>
+
+            <b-form-group label="Пароль">
+              <b-form-input placeholder="Введите пароль" />
+            </b-form-group>
+
+            <b-form-group label="Пароль">
+              <b-form-input placeholder="Повторите пароль" />
+            </b-form-group>
+
+            <div class="d-flex justify-content-center">
+              <b-button type="sumbit">Зарегистрироваться</b-button>
+            </div>
+          </b-form>
+        </b-card>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
