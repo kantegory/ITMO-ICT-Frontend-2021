defaa524db6da67c19cc81daf646fdc21cfa526d
Egor Kondrashov <e.kondr01@gmail.com>
2022-01-14 18:14:58 +0300

    feat: add getMyProfile mixin

--- course_project/K33402/Kondrashov_Egor/bookings/src/components/EditProfileMain.vue
+++ course_project/K33402/Kondrashov_Egor/bookings/src/components/EditProfileMain.vue
@@ -85,35 +85,17 @@
   </b-container>
 </template>
 <script>
+import getMyProfile from "@/mixins/getMyProfile.js"
+
 export default {
   name: "EditProfileMain",
+  mixins: [getMyProfile],
   data() {
     return {
       profile: Object
     }
   },
   methods: {
-    async getMyProfile() {
-      try {
-        const config = {
-          headers: {
-            Authorization: this.$store.getters.authHeader
-          }
-        }
-        const resp = await this.axios.get(
-          "http://localhost:8000/api/users/me",
-          config
-        )
-
-        if (resp.status !== 200) {
-          throw new Error(resp.error)
-        }
-
-        this.profile = resp.data
-      } catch (e) {
-        console.error("Error from API: ", e)
-      }
-    },
     async onSubmit(event) {
       event.preventDefault()
       try {
--- course_project/K33402/Kondrashov_Egor/bookings/src/views/MyProfile.vue
+++ course_project/K33402/Kondrashov_Egor/bookings/src/views/MyProfile.vue
@@ -8,36 +8,14 @@
 <script>
 import AppHeader from "../components/AppHeader"
 import MyProfileMain from "../components/MyProfileMain.vue"
+import getMyProfile from "@/mixins/getMyProfile.js"
 
 export default {
   name: "MyProfile",
-
+  mixins: [getMyProfile],
   components: {
     AppHeader,
     MyProfileMain
-  },
-  methods: {
-    async getMyProfile() {
-      try {
-        const config = {
-          headers: {
-            Authorization: this.$store.getters.authHeader
-          }
-        }
-        const resp = await this.axios.get(
-          "http://localhost:8000/api/users/me",
-          config
-        )
-
-        if (resp.status !== 200) {
-          throw new Error(resp.error)
-        }
-
-        this.profile = resp.data
-      } catch (e) {
-        console.error("Error from API: ", e)
-      }
-    }
   },
   beforeMount() {
     this.getMyProfile()
