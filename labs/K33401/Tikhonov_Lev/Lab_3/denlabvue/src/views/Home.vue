2c3b0c8e8177d456b5c7aad9f6216ea98eaf9a60
Lev <tikhonov.lev0@gmail.com>
2022-01-23 21:31:32 +0400

    add working project

---
+++ labs/K33401/Tikhonov_Lev/Lab_3/denlabvue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Registration/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Registration from '../components/Registration'
+
+export default {
+  name: 'Home',
+  components: {
+    Registration
+  }
+}
+</script>
