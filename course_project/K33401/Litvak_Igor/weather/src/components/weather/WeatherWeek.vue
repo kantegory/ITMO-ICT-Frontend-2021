cfafd511fa62dbdb6535d76b9414e957d4c9d035
igorlitvak <igr.litvak@gmail.com>
2022-01-24 22:43:39 +0300

    Move files from LW3

---
+++ course_project/K33401/Litvak_Igor/weather/src/components/weather/WeatherWeek.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p><b>{{ day }}</b></p>
+        <p>{{ temp }}℃</p>
+        <div class="week-img-container">
+            <img :src="icon" :alt="desc" class="img-icon">
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WeatherWeek",
+    props: ["icon", "temp", "desc", "day"]
+}
+</script>
+
+<style scoped>
+
+</style>
