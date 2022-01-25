2cd2e95b4c9c4adfd71a37cd1137f66d5b67cd9f
Ivan Kostylev <jkostylev47@gmail.com>
2022-01-25 16:57:04 +0300

    Implemented page with login.

--- course_project/K33401/Kostylev_Ivan/weather_vue/src/main.js
+++ course_project/K33401/Kostylev_Ivan/weather_vue/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
@@ -8,3 +12,8 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
--- course_project/K33401/Kostylev_Ivan/weather_vue/src/router/index.js
+++ course_project/K33401/Kostylev_Ivan/weather_vue/src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SignIn from '../views/Signin.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'SignIn',
+    component: SignIn
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
