bd171b0ba018c8e06408195cb2aed7dbe9944020
semenkulemin <skulemin@icloud.com>
2022-01-25 21:53:37 +0300

    update WeatherCards and store

--- labs/K33401/Kulemin_Semyon/LW3/lab3/src/store/index.js
+++ labs/K33401/Kulemin_Semyon/LW3/lab3/src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    cities: [],
+    cities: [['Moscow', '55,7522', '37,6156'], ['LA', '34.0522', '-118.244'], ['London', '51.5058', '-0.12574'], ['NY', '40.7143', '-74.006']],
     weather: []
   },
   getters: {
@@ -19,11 +19,6 @@ export const store = new Vuex.Store({
   mutations: {
     setCities (state, payload) {
       state.cities = payload
-    }
-  },
-  actions: {
-    getCities ({ commit }) {
-      commit('setCities', [['Moscow', '55,7522', '37,6156'], ['LA', '34.0522', '-118.244'], ['London', '51.5058', '-0.12574'], ['NY', '40.7143', '-74.006']])
     }
   }
 })
