bfb9c9b3f7bc056f66b1946657a97639091aa511
meowberry <ann.matrokhina@gmail.com>
2022-01-25 17:50:33 +0300

    Set new url

--- course_project/K33402/Matrokhina_Anna/src/mixins/WeatherMixin.vue
+++ course_project/K33402/Matrokhina_Anna/src/mixins/WeatherMixin.vue
@@ -27,70 +27,61 @@ export default {
     },
 
     async resolveCityName(city) {
-      let result = {status: true, data: null, error: null}
+      let result = { status: true, data: null, error: null }
 
-      let url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${this.openweathermapKey}`
-      await this.axios.get(url)
-          .then(response => {
-            if (response.data.length === 0) {
-              result.status = false
-              result.error = 'Ошибка! Указанный город не найден'
-            } else {
-              result.data = response.data[0]
-            }
-          })
-          .catch(error => {
-            result.status = false
-            result.error = 'Ошибка! Указанный город не найден'
-          })
+      let url = `http://127.0.0.1:8000/city/?search=${city}&appid=${this.openweathermapKey}`
+
+      try {
+        const response = await this.axios.get(url)
+
+        if (response.data.length === 0) {
+          result.status = false
+          result.error = 'Ошибка! Указанный город не найден'
+        } else {
+          result.data = response.data
+        }
+      } catch (e) {
+        result.status = false
+        result.error = 'Ошибка! Указанный город не найден'
+      }
 
       return result
     },
 
     async apiCityWeather(city, date) {
-      let result = {status: true, data: null, error: null}
+      let result = { status: true, data: null, error: null }
 
       let cityResolved = await this.resolveCityName(city)
 
-      if (!cityResolved.status) {
-        return cityResolved
+      let url = `http://127.0.0.1:8000/weather/?city=${cityResolved.data.id}`
+
+      try {
+        const response = await this.axios.get(url)
+
+        if (date === 'today') {
+          result.data = response.data.current
+        } else if (date === 'tomorrow') {
+          result.data = response.data.daily[1]
+          result.data.temp = result.data.temp.day
+        } else if (date === 'weekends') {
+          let today = new Date().getDay()
+          let weekend = 7
+          let diff = weekend - today
+
+          if (diff === 0) {
+            diff = 7
+          }
+
+          result.data = response.data.daily[diff]
+          result.data.temp = result.data.temp.day
+        }
+
+        result.data.name = cityResolved.data.local_names.ru
+      } catch (e) {
+        console.log('error getting weather', e)
+        result.status = false
+        result.error = 'Ошибка! Не удалось получить прогноз погоды'
       }
-
-      let params = new URLSearchParams()
-      params.append('lat', cityResolved.data.lat)
-      params.append('lon', cityResolved.data.lon)
-      params.append('exclude', 'hourly,minutely')
-      params.append('units', 'metric')
-      params.append('appid', this.openweathermapKey)
-
-      let url = `https://api.openweathermap.org/data/2.5/onecall`
-      await this.axios.get(url, {params: params})
-          .then(response => {
-            if (date === 'today') {
-              result.data = response.data.current
-            } else if (date === 'tomorrow') {
-              result.data = response.data.daily[1]
-              result.data.temp = result.data.temp.day
-            } else if (date === 'weekends') {
-              let today = new Date().getDay()
-              let weekend = 7
-              let diff = weekend - today
-
-              if (diff === 0) {
-                diff = 7
-              }
-
-              result.data = response.data.daily[diff]
-              result.data.temp = result.data.temp.day
-            }
-
-            result.data.name = cityResolved.data.local_names.ru
-          })
-          .catch(error => {
-            console.log('error getting weather', error)
-            result.status = false
-            result.error = 'Ошибка! Не удалось получить прогноз погоды'
-          })
 
       return result
     }
