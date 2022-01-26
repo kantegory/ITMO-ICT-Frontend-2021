10a22e6b0b5c774d1f48b5605a8e1abf3f6c2600
Nikolay Prokhorov <mail@prokhn.ru>
2022-01-26 05:26:24 +0300

    Fix typescript

--- course_project/K33402/Prokhorov_Nikolay/mixins/ToastsMixin.ts
+++ course_project/K33402/Prokhorov_Nikolay/mixins/ToastsMixin.ts
@@ -1,4 +1,13 @@
 import { Component, Vue } from "nuxt-property-decorator";
+import { BvModal } from "bootstrap-vue/src/components/modal";
+import { BvToast } from "bootstrap-vue/src/components/toast";
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    readonly $bvModal: BvModal
+    readonly $bvToast: BvToast
+  }
+}
 
 @Component
 export default class ToastsMixin extends Vue {
